9f48e0ea5c149506daa3e763228fad7b7789fcbf
Jiri Kremser <jiri.kremser@gmail.com>
2022-11-22 16:08:32 +0100

    Add license headers
    Signed-off-by: Jiri Kremser <jiri.kremser@gmail.com>

--- pkg/key/key.go
+++ pkg/key/key.go
@@ -1,11 +1,22 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package key
 
 const (
-	ClusterTagPrefix     = "giant_swarm_cluster"
 	CapiClusterLabelKey  = "cluster.x-k8s.io/cluster-name"
 	CleanerFinalizerName = "cluster-api-cleaner-cloud-director.finalizers.giantswarm.io"
-
-	LoadBalancerProvisioningStatusActive = "ACTIVE"
-	LoadBalancerProvisioningStatusError  = "ERROR"
-	VolumeStatusDeleting                 = "deleting"
 )
